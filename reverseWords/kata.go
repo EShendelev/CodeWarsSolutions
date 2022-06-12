cd6267894ce666cd5f0a8aa436ba9d3205077f78
EShendelev <sendeleve@yandex.ru>
2022-06-12 16:01:07 +0300

    reverse words solution without double spaces

---
+++ reverseWords/kata.go
@@ -0,0 +1,21 @@
+package kata
+
+import "strings"
+
+func ReverseWords(str string) string {
+	arrStr := strings.Fields(str)
+	for i, v := range arrStr {
+		word := strings.Split(v, "")
+		word = reverseStr(word)
+		arrStr[i] = strings.Join(word, "")
+	}
+	res := strings.Join(arrStr, " ")
+	return res
+}
+
+func reverseStr(str []string) []string {
+	for i := 0; i < len(str)/2; i++ {
+		str[i], str[len(str)-1-i] = str[len(str)-1-i], str[i]
+	}
+	return str
+}
