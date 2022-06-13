329d7e6ba400affbc859d7a78f43d344f6e1e1b8
EShendelev <sendeleve@yandex.ru>
2022-06-13 21:15:45 +0300

    ...

--- sandbox.go
+++ sandbox.go
@@ -1,36 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"strings"
-)
-
-// func main() {
-// 	str := ReverseWords("The quick brown fox jumps over the lazy dog.")
-// 	fmt.Println(str)
-// }
-
 func main() {
-	str := "stressed   desserts"
 
-	arrStr := strings.Fields(str)
-	fmt.Printf("%#+v\n", arrStr)
-	for i, v := range arrStr {
-		word := strings.SplitAfter(v, " ")
-		word = reverseStr(word)
-		arrStr[i] = strings.Join(word, "")
-		fmt.Printf("%#+v\n", v)
-	}
-	res := strings.Join(arrStr, " ")
-	fmt.Printf("%#+v\n", res)
-
-	//strings.ReplaceAll(resStr, "_", " ")
-	// return resStr // reverse those words
+  count := 0
+  for _, i := range s {
+     count += int(i) - 'a' + 1;
+  }
+  return count
 }
-
-func reverseStr(str []string) []string {
-	for i := 0; i < len(str)/2; i++ {
-		str[i], str[len(str)-1-i] = str[len(str)-1-i], str[i]
-	}
-	return str
 }
